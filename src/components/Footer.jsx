eedf774465ce968ff369be40f8f59f9fd3fbf9f3
AbdallhElzayat2020 <https://github.com/AbdallhElzayat2020/E-commerce-React-App>
2023-08-05 04:01:29 +0300

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="mb-0 text-center">
+        <div className="d-flex align-items-center justify-content-center pb-5">
+          <div className="col-md-6">
+            <p className="mb-3 mb-md-0">Developed❤️ by
+              <a href="https://api.whatsapp.com/send?phone=201212484233" className=" text-dark fs-5" target="_blank" rel="noreferrer">Abdallh Elzayat</a>
+            </p>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
